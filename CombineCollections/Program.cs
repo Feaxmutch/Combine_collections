72869c54aa140bba4b31103b620c921edc4e7b84
Feaxmutch <feaxmutch@gmail.com>
2024-10-20 23:05:02 +0300

    Initial commit

---
+++ CombineCollections/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CombineCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
