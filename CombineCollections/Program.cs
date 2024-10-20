81718398c4fd6c06df6667fdf72d6ee630d207a1
Feaxmutch <feaxmutch@gmail.com>
2024-10-20 23:17:09 +0300

    Implemented collections combining

--- CombineCollections/Program.cs
+++ CombineCollections/Program.cs
@@ -2,9 +2,40 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers1 = new() { 6, 5, 2 };
+            List<int> numbers2 = new() { 6, 9, 2 };
+            List<int> numbers3 = new() { 3, 4, 6 };
+            List<int> numbers4 = new() { 3, 1, 3 };
+            List<int>[] allNumbers = { numbers1, numbers2, numbers3, numbers4 };
+
+            List<int> combinedNumbers = CombineCollections(allNumbers);
+
+            foreach (var number in combinedNumbers)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static List<int> CombineCollections(List<int>[] collections)
+        {
+            List<int> combinedCollection = new();
+
+            foreach (var numbers in collections)
+            {
+                foreach (var number in numbers)
+                {
+                    if (combinedCollection.Contains(number) == false)
+                    {
+                        combinedCollection.Add(number);
+                    }
+                }
+            }
+
+            return combinedCollection;
         }
     }
 }
